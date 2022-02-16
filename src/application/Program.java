f902b15e6d6290da8d7de6c84effd87773eee06b
Lucas Kaminski <lucaskaminskith@gmail.com>
2022-02-16 18:30:28 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,16 @@ package application;
 
 import java.util.Locale;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
